ce70d1cbe6b5c03885d4d0ee88425001269bc101
Oksana Zaga <oksanazag7@gmail.com>
2026-03-24 12:48:55 -0400

    rest

---
+++ rest....js
@@ -0,0 +1,10 @@
+//1
+//Write a function with rest parameters to print sightings 
+// of different animals within the sanctuary. 
+// This function should accept an arbitrary number of animal names.
+function animals(...names) {
+    console.log(names);
+    return names.animals((sum) => sum + 1)
+};
+console.log(names, sum);
+//2
